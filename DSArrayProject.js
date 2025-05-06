ea229a99543cc4615a5c9849bb62cc2c15e9343c
Euterpixel <mariarpista@gmail.com>
2025-05-06 15:37:32 +0530

    DSArray Project

--- DSArrayProject.js
+++ DSArrayProject.js
@@ -18,7 +18,8 @@ function showMenu() {
     console.log("\n---To-Do-List");
     console.log("1. Add task");
     console.log("2. Show Tasks");
-    console.log("3. Exit");
+    console.log("3. Delete Task");
+    console.log("4. Exit");
 
     rl.question("Choose an option (1-3): ", function (option) {
         switch (option) {
@@ -29,6 +30,9 @@ function showMenu() {
                 showTasks();
                 break;
             case '3':
+                deleteTask();
+                break;
+            case '4':
                 console.log("Goodbye!");
                 rl.close();
                 break;
@@ -60,5 +64,27 @@ function showTasks(task) {
         });
     }
     showMenu();
+}
+function deleteTask(number) {
+    let numberOfTask = 1;
+    if (tasks.length > 0) {
+        for (let i = 0; i < tasks.length; i++) {
+            console.log((i+1) + ". " + tasks[i]);
+        }
+        rl.question('Which task would you like to delete?', function(number){
+            const index = parseInt(number) - 1;
+            if (index >= 0 && index < tasks.length){
+                const removed = tasks.splice(index,1);
+                console.log(`Deleted: ${removed[0]}`);
+            }
+            else{
+                console.log("Invalid task number. ");
+            }
+            showMenu();
+        })
+        
+    }
+    else { console.log('There is nothing to delete') ;
+    showMenu()};
 }
 showMenu();
