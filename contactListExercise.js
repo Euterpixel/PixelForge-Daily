c7fa3accd0c7c028db1ece078c441d94238733ff
Euterpixel <mariarpista@gmail.com>
2025-05-07 23:33:50 +0530

    Add files via upload

--- contactListExercise.js
+++ contactListExercise.js
@@ -1,5 +1,4 @@
 
-// list.print();
 
 class Node {
     constructor(data) {
@@ -12,6 +11,7 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
     add(data) {
         const newNode = new Node(data);
         if (this.head === null) {
@@ -21,19 +21,37 @@ class LinkedList {
         else {
             let currentNode = this.head;
             while (currentNode.next !== null) {
-                console.log(this.head.data);
-                currentNode = currentNode.next;
-                console.log(currentNode.data);
+                if (newNode.data === "Manu" && currentNode.next.data === "Aman") {
+                    newNode.next = currentNode.next;
+                    currentNode.next = newNode;
+                    return
+                }
+                else {
+
+                    console.log(this.head.data);
+                    currentNode = currentNode.next;
+                    console.log(`inside while loop ${currentNode.data}`);
+                }
             }
             currentNode.next = newNode;
-            console.log(currentNode.next.data);
+            console.log(`outside while loop ${currentNode.next.data}`);
+        }
+    }
+    print() {
+        let currentPrintingNode = this.head;
+        while (currentPrintingNode !== null) {
+            console.log(currentPrintingNode.data);
+            currentPrintingNode = currentPrintingNode.next;
         }
     }
 }
+
 // This is the first exercise I code for learning linked lists 
 const list = new LinkedList();
 list.add("Alice");
 list.add("Bob");
 list.add("Charlie");
 list.add("Aman");
+list.add("Manu");
 list.add("Juliet");
+list.print();
